01ff36ab85777c9fc9b6933fb47001e8e82a09f4
Alejandro_v <a.vera07@ufromail.cl>
2025-02-19 13:40:26 -0300

    Terminamos los fundamentos

--- Python/Fundamentos/main.py
+++ Python/Fundamentos/main.py
@@ -1,1 +1,79 @@
+# Fundamentos de Python
 print("Hola mundo")
+
+
+### Tipos de datos ###
+
+"CHILE" # String
+
+2025 # Entero
+
+18.2 # Float
+
+True # Booleano
+
+
+### Variables ###
+
+pais = "Chile"
+
+# Datos compuestos
+
+# Listas
+lista = [1, 2, 3, 4, 5]
+print(lista)
+
+# Tuplas
+
+tupla = (1, 2, 3, 4, 5)  # Las tuplas no se pueden modificar
+print(tupla) 
+
+# Diccionarios
+
+dicccionario = {
+    "nombre": "Juan",
+    "apellido": "Perez",
+    "curso": "Python"
+}
+
+
+### Operadores ###
+
+## Aritmeticos ##
+
+# Suma y Resta
+suma = 5 + 5
+
+
+resta = 5 - 5
+
+# Multiplicacion y Division
+
+multiplicacion = 5 * 5
+
+division = 5 / 5
+
+# Resto
+
+resto = 5 % 5
+
+
+
+
+## Comparacion ##
+
+
+igualdad = 5 == 5
+
+
+distinto = 5 != 6
+
+mayor = 5 > 6
+
+menor = 5 < 6
+
+mayor_igual = 5 >= 5
+
+menor_igual = 5 <= 6
+
+
