c149d591d345c4b9f573d1290dfe5ad549b136cb
Alejandro_v <a.vera07@ufromail.cl>
2025-03-24 21:42:52 -0300

    Agregamos avances en la app para poner un punto en el mapa creado

--- Python/08-App-Mapping/main.py
+++ Python/08-App-Mapping/main.py
@@ -1,10 +1,20 @@
 import folium
+print(folium.__version__)
 import os
 
 # Crear un mapa centrado en las coordenadas proporcionadas
-mapa = folium.Map(location=[-41.46067771036199, -72.95166123635632], zoom_start=13)
+mapa = folium.Map(location=[-41.46067771036199, -72.95166123635632], zoom_start=13) 
 
-# Guardar el mapa en un archivo HTML
+folium.TileLayer(
+    tiles='https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg',
+    attr='Map tiles by Stamen Design, CC BY 3.0 — Map data © OpenStreetMap contributors',
+    name='Stamen Terrain'
+).add_to(mapa)
+
+
+add_child = folium.Marker(location=[-41.46067771036199, -72.95166123635632], 
+                          popup='Casa de la Cultura', icon= folium.Icon(color="blue")).add_to(mapa)
+
 # Obtener la ruta del archivo actual
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
