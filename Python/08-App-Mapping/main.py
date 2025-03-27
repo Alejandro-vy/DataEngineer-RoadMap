edc6d6d17348c1420e50dcfc7548357c556ef244
Alejandro_v <a.vera07@ufromail.cl>
2025-03-26 21:56:31 -0300

    Agregamos marcadores en los puntos donde se encuentran los volcanes

--- Python/08-App-Mapping/main.py
+++ Python/08-App-Mapping/main.py
@@ -1,19 +1,53 @@
 import folium
-print(folium.__version__)
+#print(folium.__version__)
 import os
 
+import pandas 
+
+data = pandas.read_csv("c:/Users/janov/Desktop/DataEngineer-RoadMap/Python/08-App-Mapping/Files/Volcanoes.txt")
+
+lat = list(data["LAT"])
+
+lon = list(data["LON"])
+
 # Crear un mapa centrado en las coordenadas proporcionadas
-mapa = folium.Map(location=[-41.46067771036199, -72.95166123635632], zoom_start=13) 
+mapa = folium.Map(location=[38.58, -99.09], zoom_start=10) 
+
+fg = folium.FeatureGroup(name="Mi Mapa")
+
+
 
 folium.TileLayer(
-    tiles='https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg',
-    attr='Map tiles by Stamen Design, CC BY 3.0 — Map data © OpenStreetMap contributors',
+     tiles='https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg',
+     attr='Map tiles by Stamen Design, CC BY 3.0 — Map data © OpenStreetMap contributors',
     name='Stamen Terrain'
-).add_to(mapa)
+ ).add_to(mapa)
 
 
-add_child = folium.Marker(location=[-41.46067771036199, -72.95166123635632], 
-                          popup='Casa de la Cultura', icon= folium.Icon(color="blue")).add_to(mapa)
+
+
+
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup='marker', icon=folium.Icon(color="blue")))
+
+
+
+
+mapa.add_child(fg)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Obtener la ruta del archivo actual
 current_dir = os.path.dirname(os.path.abspath(__file__))
