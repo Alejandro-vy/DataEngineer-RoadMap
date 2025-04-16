4a2c807c38f21d82a54d73068be9a07c4ef8bd61
Alejandro_v <a.vera07@ufromail.cl>
2025-04-15 21:06:47 -0400

    Aprendemos las sentencias limit, null y min_max

---
+++ SQL/Statements/08-null.sql
@@ -0,0 +1,4 @@
+SELECT * FROM marcas WHERE ano_creacion IS NULL; # Trae los registros donde el campo ano_creacion es nulo
+
+
+SELECT * FROM marcas WHERE ano_creacion IS NOT NULL; # Trae los registros donde el campo ano_creacion no es nulo
---
+++ SQL/Statements/09-min_max.sql
@@ -0,0 +1,3 @@
+SELECT MAX(ano_creacion) FROM marcas; # Trae el año de creación más reciente
+SELECT MIN(ano_creacion) FROM marcas; # Trae el año de creación más antiguo
+
